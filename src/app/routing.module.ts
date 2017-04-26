c3f3fd0f45329016246e172c2a5c5e577d6af362
“kVadm” <1408vk@gmail.com>
2017-04-27 02:24:12 +0300

    new mobile view

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,14 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewPostComponent }   from './main-section/new-post/new-post.component';
 import { PostComponent }      from './main-section/post-view/post-view.component';
 import { EditPostComponent }      from './main-section/edit-post/edit-post.component';
-import { PostviewContentComponent }  from './postview-content/postview-content.component';  
+import { PostviewContentComponent }  from './postview-content/postview-content.component'; 
+import { SidemenuComponent }  from './sidemenu/sidemenu.component';  
+ 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',     component: PostviewContentComponent },
   { path: 'newpost',  component: NewPostComponent },
   { path: 'post/:id', component: PostComponent },
-  { path: 'editpost/:id', component: EditPostComponent }  
+  { path: 'editpost/:id', component: EditPostComponent }, 
+  { path: 'sidemenu', component: SidemenuComponent },
 ];
 
 @NgModule({
